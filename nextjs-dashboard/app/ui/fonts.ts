03c35ab27c6e3b23d0d340a699f15e6dab9aaae6
Nayara <garciatarraganayara@gmail.com>
2025-01-22 13:32:20 +0100

    hola

---
+++ nextjs-dashboard/app/ui/fonts.ts
@@ -0,0 +1,6 @@
+import {Inter, Lusitana} from 'next/font/google';
+export const inter= Inter({subsets:['latin']});
+export const luisita=Lusitana({
+    weight:['400','700'],
+    subsets:['latin']
+});
